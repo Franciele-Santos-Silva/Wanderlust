d01c1102a76cffc7d303361dba69934cc8dacd17
Franciele-Santos-Silva <franciellysantos876@gmail.com>
2025-09-21 17:23:00 -0300

    Alteração em page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
 import Image from "next/image";
+import airplaneSvg from '../assets/airplane.svg'
 
 export default function Home() {
   return (
     <main>
-      <section className="h-screen w-full">
+      <section className="mx-auto flex h-screen w-full items-center max-w-screen-lg items-center justyfy-between" >
+        <div>
+          <h1 className="text-left text-5xl font-black">
+            Sua próxima viagem mais{' '} 
+            <span className="text-fuchsia-500">eficiente</span></h1>
+        </div>
         <Image 
-        src='/airplane.svg' 
-        alt='Franciele'
-        width={580}
-        height={580}/>
+        className="w-full max-w-2xl"
+        src={airplaneSvg}
+        alt='Franciele'/>
       </section>
     </main>
 
